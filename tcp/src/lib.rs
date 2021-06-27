bb031816907ece0fb43dde992a27f8bd6343b879
Jake Kile <jkile9@gmail.com>
2021-06-26 18:17:38 -0700

    began creating tcp connection

--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -1,11 +1,25 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+
+pub fn init_listener() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    match listener.accept() {
+        Ok((_socket, addr)) => println!("New client: {:?}", addr),
+        Err(e) => println!("Couldn't get client: {:?}", e),
+    }
+}
+
+pub fn init_connection(addr: SocketAddrV4) {
+    if let Ok(_) = TcpStream::connect(addr) {
+        println!("Connected to the server!");
+    } else {
+        println!("Could not connect to server");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        super::init_connection();
     }
-}
-
-pub fn run_tcp() {
-    println!("Hello from TCP")
 }
