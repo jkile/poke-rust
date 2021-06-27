df453d8760d15dbb2b13e646b5af4be4dad521ed
Jake Kile <jkile9@gmail.com>
2021-06-26 18:42:52 -0700

    removed tcp server to be moved to go server at a later date

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,6 @@
 use tcp;
-use std::net::SocketAddrV4;
-use std::net::Ipv4Addr;
+
 
 fn main() {
-    tcp::init_listener();
-    // let host = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
-    // tcp::init_connection(host);
+
 }
--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -1,12 +1,12 @@
-use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+use std::net::{ SocketAddrV4, TcpStream };
 
-pub fn init_listener() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("New client: {:?}", addr),
-        Err(e) => println!("Couldn't get client: {:?}", e),
-    }
-}
+// pub fn init_listener() {
+//     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+//     match listener.accept() {
+//         Ok((_socket, addr)) => println!("New client: {:?}", addr),
+//         Err(e) => println!("Couldn't get client: {:?}", e),
+//     }
+// }
 
 pub fn init_connection(addr: SocketAddrV4) {
     if let Ok(_) = TcpStream::connect(addr) {
@@ -20,6 +20,6 @@ pub fn init_connection(addr: SocketAddrV4) {
 mod tests {
     #[test]
     fn it_works() {
-        super::init_connection();
+        
     }
 }
